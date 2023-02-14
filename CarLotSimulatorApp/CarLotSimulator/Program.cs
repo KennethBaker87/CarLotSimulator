54331613e3dd7b25cfe8ebde25bb8666170a5b85
Kenneth Baker <spyksaturn@gmail.com>
2023-02-14 09:45:30 -0500

    Initial Commit

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
@@ -17,7 +18,37 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            Car dodge = new Car();
+            dodge.Year = 2006;
+            dodge.Make = "Dodge";
+            dodge.Model = "Challenger";
+            dodge.EngineNoise = Car.MakeEngineNoise("loud");
+            dodge.HonkNoise = Car.MakeHonkNoise("loud");
+            dodge.IsDrivable = true;
 
+            var ford = new Car()
+            {
+                Year = 1996,
+                Make = "Ford",
+                Model = "Pinto",
+                EngineNoise = Car.MakeEngineNoise("soft"),
+                HonkNoise = Car.MakeHonkNoise("soft"),
+                IsDrivable = false,
+            };
+
+            var chevy = new Car(2020, "Chevy", "Camero", Car.MakeEngineNoise("loud"), Car.MakeHonkNoise("loud"), true);
+            var carList = new List<Car> { dodge, ford, chevy };
+            
+            
+
+            
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"Year: {vehicle.Year}\nMake: {vehicle.Make}\nModel: {vehicle.Model}\nEngine Noise: {vehicle.EngineNoise}\n" +
+                    $"Horn Noise: {vehicle.HonkNoise}\nDrivable: {vehicle.IsDrivable}\n");
+            }
+
+            
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
